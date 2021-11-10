87df0791d73b8c7dcf889bf55a560807d6d4cad0
mangoppola <50957324+mangoppola@users.noreply.github.com>
2021-11-10 21:08:49 +0100

    Update plot.py
    1. Aggiunta l'etichetta ai dati sperimentali (vedi riga 18)
    2. Corretta la descrizione di alcune cose in inizializzazione grafico
    3. Grande modifica in fit: aggiunta la possibilitá di mostrare i parametri di fit in un box. Sono state aggiunte le dovute descrizioni
    ATTENZIONE! Questo aggiornamento rompe la compatibilitá con versioni precedenti di funzioni_secondarie.py. Aggiornare entrambi

--- plot.py
+++ plot.py
@@ -6,15 +6,16 @@
 import scipy.odr
 import numpy as np
 from matplotlib import pyplot as plt
-from funzioni_secondarie import fileselect, print_plot, print_params, curve_fit
+from funzioni_secondarie import fileselect, print_plot, curve_fit, parameters_text
 
 ##SELEZIONE FILE
 nome = fileselect()
 
-##INIZIALIZZAZIONE GRAFICO
-#ecolor: colore punti, fmt: forma punti, barsabove: metti a True se non si vede l'errore, ms: dimensione punti sperimentali
-x, xerror, y, yerror= np.loadtxt(nome, unpack=True, delimiter = ";", skiprows=1)
-plt.errorbar(x,y,xerr=xerror, yerr=yerror, fmt ="o", ecolor="black", barsabove=False, ms= 2)
+# INIZIALIZZAZIONE GRAFICO
+# @ecolor colore barre di errore, @fmt forma punti, @barsabove metti a True se non si vede l'errore @ms: dimensione punti sperimentali
+x, xerror, y, yerror = np.loadtxt(nome, unpack=True, delimiter=";", skiprows=1)
+plt.errorbar(x, y, xerr=xerror, yerr=yerror, fmt="o", ecolor="black",
+             barsabove=False, ms=0.5, label="Dati Sperimentali")
 
 ##MODIFICARE SE VUOI UN FIT
 #esempio di modello: beta[0]+beta[1]*np.cos(x**2)
@@ -25,7 +26,21 @@ initial_guess = [0,0,0]
 selection = input("Hai bisogno di fare un fit? [y o invio per confermare]: ")
 if selection == "y" or selection == "":
     data = [x,y,xerror,yerror]
-    curve_fit(model,initial_guess,data)
+    fit_parameters = curve_fit(model, initial_guess, data)
+    
+    # aggiungo i parametri del fit come testo
+    # modificare names per mettere i giusti nomi dei parametri del fit
+    # decimal places serve a dare il numero di cifre decimali
+    text = parameters_text(fit_parameters[0], fit_parameters[1],
+                           names=("beta0", "beta1", "beta2"), decimal_places = 3,
+                           notab=True, print_text=False)
+    
+    #disegno il testo su schermo. @xy imposta l'angolo dove mettere il testo. 0 e 1 rappresentano gli estremi degli assi, con (0,0) in basso a sinistra e (1,1) in alto a destra
+    #@xytext serve a spostare la casella di tot punti rispetto al punto selezionato. Regolare per spostare la casella attorno ad un angolo
+    plt.gca().annotate(text, xy=(0,1), xytext=(15,-30), fontsize = 10,
+                     xycoords='axes fraction', textcoords='offset points',
+                     bbox=dict(facecolor='none', edgecolor = "black"),
+                     horizontalalignment = "left", verticalalignment ="bottom")
     
 ##DEFINIZIONE ETICHETTE
 plt.title("Grafico")
