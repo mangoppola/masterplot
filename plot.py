7a22fdced059d2030b1c340dd667e2e221b7fc9d
mangoppola <50957324+mangoppola@users.noreply.github.com>
2021-11-11 12:34:53 +0100

    Update plot.py
    Aggiunto il comando per generare la legenda in parametri opzionali

--- plot.py
+++ plot.py
@@ -54,7 +54,8 @@ plt.ylabel("Asse y [udm]")
 #plt.xscale("Log")
 ##Questo serve per scegliere range e scansione degli assi
 #plt.yticks(np.arange(ymin, ymax, larghezza_tacche))
-
+##Togli il commento per aggiungere la legenda
+#plt.legend()
 
 ##Stampa plot e mostralo in console
 print_plot(nome)
