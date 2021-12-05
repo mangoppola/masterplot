68dd96946a5b005931765c5f4d3e7d5ee2227e08
mangoppola <50957324+mangoppola@users.noreply.github.com>
2021-12-05 17:43:45 +0100

    Update multiplot.py
    1. Sistemati i nomi delle funzioni importate
    2. A riga 20 c'erano parentesi quadre invece di tonde
    3. A riga 26-27 c'erano due comandi che facevano la stessa cosa, solo che il secondo non metteva gli errori. Di conseguenza, venivano generati due plot identici e sovrapposti e, in base a quello che veniva mostrato sopra, poteva esserci discrepanza tra i colori della legenda e i colori del grafico.

--- multiplot.py
+++ multiplot.py
@@ -6,7 +6,7 @@
 import scipy.odr
 import numpy as np
 from matplotlib import pyplot as plt
-from funzioni_secondarie import fileselect, print_plot, print_params, curve_fit
+from funzioni_secondarie import fileselect, print_plot, curve_fit, parameters_text
 
 ##SELEZIONE FILE
 n_graphs = int(input("Quanti grafici vuoi rappresentare? "))
@@ -17,14 +17,14 @@ for i in range(n_graphs):
     file = fileselect()
     datasets.append(file)
     label = input("Cosa rappresenta questo dataset? ")
-    labels.append[label]
+    labels.append(label)
 
 for i in range(len(datasets)):
     #estraggo i dati dal dataset
     data = np.loadtxt(datasets[i], unpack=True, delimiter = ";", skiprows=1)
-    # IMPORTANTISSIMO: QUI SI ASSUME CHE TU STIA CARICANDO IN ORDINE X, Y, ERRX E ERRY
-    plt.errorbar(data[0],data[2],xerr=data[1],yerr=data[3],fmt ="o", ecolor="black", barsabove=False, ms= 2)
-    plt.plot(data[0],data[2], label = labels[i])
+    # IMPORTANTISSIMO: QUI SI ASSUME CHE TU STIA CARICANDO IN ORDINE X, ERRX, Y E ERRY
+    plt.errorbar(data[0],data[2],xerr=data[1],yerr=data[3],fmt ="o", ecolor="black", barsabove=False, ms= 1, label = labels[i])
+
     
 plt.legend()
 
